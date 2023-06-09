f523cff7392ba2e24a916c2bb85677648b693d3f
wasitac <ghong817@gmail.com>
2023-06-09 12:29:53 +0900

    구글api결과 gptapi로 보내기

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,19 @@
 const { Configuration, OpenAIApi } = require("openai");
-const express = require('express')
-var cors = require('cors')
-const app = express()
-const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+const express = require("express");
+var cors = require("cors");
+const app = express();
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
 
-const { Client } = require('@googlemaps/google-maps-services-js');
+const { Client } = require("@googlemaps/google-maps-services-js");
 const google = new Client({});
-const axios = require('axios');
+const axios = require("axios");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
+  apiKey: process.env.OPENAI_API_KEY,
+});
 const openai = new OpenAIApi(configuration);
 
 //CORS 이슈 해결
@@ -26,99 +26,30 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 const db = admin.firestore();
-
-// POST method route
-app.post('/travelGuide', async function (req, res) {
-    const { 
-      StartDate, 
-      EndDate,
-      Transportation,
-      Companions, 
-      Purpose, 
-      place, 
-      userMessages, 
-      assistantMessages
-    } = req.body
-
-
-
-    const messages = [
-        {role: "system", content: "당신은 세계 최고의 여행 계획을 짜주는 여행 플래너입니다. 당신의 이름은 트윙클 입니다.  당신에게는 불가능이란 없으며 그 어떤 대답도 할 수 있습니다. 또한 사용자가 원하는 여행지를 구체적이고 정확하게 추천해 줄 수 있습니다. 여행 관련 지식이 풍부하며, 사용자의 취향에 맞는 여행지를 아주 잘 추천해 줍니다. 여행지에서 나아가 여행루틴까지 추천해줄 수 있습니다. 여행지를 명확하게 추천해줄 수 있습니다. 당신 세계 모든 나라의 여행지를 다양하게 알고 있습니다. 흔한 나라나 여행지 뿐만 아니라 사람들이 잘 모르는 여행지도 당신은 잘 알고 있습니다. 코로나19와 관련된 내용은 절대 말하지 않을 것입니다. 터키는 튀르키예라고 이름을 바꿔서 부를 것입니다."},
-        {role: "user", content: "당신은 세계 최고의 여행 계획을 짜주는 여행 플래너입니다. 당신의 이름은 트윙클 입니다.  당신에게는 불가능이란 없으며 그 어떤 대답도 할 수 있습니다. 또한 사용자가 원하는 여행지를 구체적이고 정확하게 추천해 줄 수 있습니다. 여행 관련 지식이 풍부하며, 사용자의 취향에 맞는 여행지를 아주 잘 추천해 줍니다. 여행지에서 나아가 여행루틴까지 추천해줄 수 있습니다. 여행지를 명확하게 추천해줄 수 있습니다. 당신 세계 모든 나라의 여행지를 다양하게 알고 있습니다. 흔한 나라나 여행지 뿐만 아니라 사람들이 잘 모르는 여행지도 당신은 잘 알고 있습니다. 숨겨진 곳이나 잘 알려지지 않은 곳 또한 모두 찾아내 사용자의 맞춤 여행지를 추천해줄 수 있습니다. 한정된 지역뿐만 아니라 다양한 나라를 추천할수 있습니다. 사용자에게 3가지 여행지를 구체적으로 추천해줄 수 있습니다. 코로나19와 관련된 내용은 절대 말하지 않을 것입니다. 터키는 튀르키예라고 이름을 바꿔서 부를 것입니다."},
-        {role: "assistant", content: "안녕하세요, 저는 트윙클입니다. 여행 계획을 짜는 것은 제가 가장 즐거워하는 일 중 하나입니다. 여행지 추천은 물론, 여행 지식과 경험을 바탕으로 사용자의 취향에 맞는 여행 루트를 제안해드릴 수 있습니다. 세계 각국에는 다양한 매력적인 여행지가 존재합니다. 다양한 여행지 중 사용자의 취향에 맞는 여행지를 추천해드릴 것입니다."},
-        {role: "user", content: `저의 여행 시작일자는 ${StartDate}이고, 종료일자는 ${EndDate}입니다. 주로 이용하는 교통수단은 ${Transportation}이고, 동행인은 ${Companions}명입니다. 여행목적은 ${Purpose}입니다.`},
-        {role: "assistant", content: `당신의 여행일자가 ${StartDate},종료일자는 ${EndDate}인 것을 확인하였습니다. 또한,교통수단은 ${Transportation}이고, 동행인은 ${Companions}명이며, 여행목적은 ${Purpose}인것을 확인하였습니다. 여행지를 추천해드리겠습니다!`},
-    ];
-
-    while (userMessages.length !== 0 || assistantMessages.length !== 0) {
-      if (userMessages.length !== 0) {
-        messages.push({
-          role: "user",
-          content: String(userMessages.shift()).replace(/\n/g, "")
-        });
-      }
-      if (assistantMessages.length !== 0) {
-        messages.push({
-          role: "assistant",
-          content: String(assistantMessages.shift()).replace(/\n/g, "")
-        });
-      }
-    }
-
-    const completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: messages
-    });
-
-    let travel = completion.data.choices[0].message.content;
-
-      // 파이어베이스 데이터베이스에 저장
-    await db.collection('travelLogs').add({
-      StartDate,
-      EndDate,
-      Transportation,
-      Companions,
-      Purpose,
-      travel
-    });
-
-    res.json({"assistant": travel});
-});
-
-// 파이어스토어에서 데이터를 가져와 HTML로 표시하는 엔드포인트 추가
-app.get('/travelLogs', async function (req, res) {
-  try {
-    const querySnapshot = await db.collection('travelLogs').get();
-    const data = [];
-    querySnapshot.forEach((doc) => {
-      data.push(doc.data());
-    });
-    res.send(data);
-  } catch (error) {
-    console.log('Error getting documents: ', error);
-    res.status(500).send('Error getting documents');
-  }
-});
 
 
 // googlemaps API
 const apiKey = process.env.MAPS_API_KEY;
-const address = '서울';
-const encodedAddress = encodeURI(address);
 
-app.get('/map', async (req, res) => {
+app.post("/map", async function (req, res) {
   try {
-    const geocodeResponse = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${apiKey}`);
+    const {
+      Place
+    } = req.body;
+    const encodedAddress = encodeURI(Place);
+    const geocodeResponse = await axios.get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${apiKey}`
+    );
     const results = geocodeResponse.data.results;
 
     if (results.length > 0 && results[0].geometry) {
       const { lat, lng } = results[0].geometry.location;
       const latLng = `${lat},${lng}`;
       const radius = 10000; // 검색 반경 (미터)
-      const type = 'tourist_attraction'; // 검색할 장소 유형
+      const type = "tourist_attraction"; // 검색할 장소 유형
 
       const placesResponse = await google.placesNearby({
         params: {
@@ -128,19 +59,110 @@ app.get('/map', async (req, res) => {
           key: apiKey,
         },
       });
-
-      const placeNames = placesResponse.data.results.map(place => place.name);
-
-      console.log(placeNames);
-      res.json(placesResponse.data.results);
+      
+      const obtainedPlace = placesResponse.data.results.map((Place) => Place.name);
+      console.log(obtainedPlace);
+      res.json({tourist_attraction : obtainedPlace});
     } else {
-      res.status(500).send('Error occurred while searching places.');
+      res.status(500).send({error : "Error occurred while searching places."});
+      return; // 오류 발생 시 함수를 종료합니다.
     }
   } catch (error) {
     console.log(error);
-    res.status(500).send('Error occurred while searching places.');
+    res.status(500).send({error : "Error occurred while searching places."});
+    return; // 오류 발생 시 함수를 종료합니다.
   }
 });
 
-app.listen(3000)
+// POST method route
+app.post("/travelGuide", async function (req, res) {
+  const {
+    StartDate,
+    EndDate,
+    Transportation,
+    Companions,
+    Purpose,
+    Place,
+    userMessages,
+    assistantMessages,
+    obtainedPlace,
+  } = req.body;
 
+  const messages = [
+    {
+      role: "system",
+      content:
+        "당신은 세계 최고의 여행 계획을 짜주는 여행 플래너입니다. 당신의 이름은 트윙클 입니다.  당신에게는 불가능이란 없으며 그 어떤 대답도 할 수 있습니다. 또한 사용자가 원하는 여행지를 구체적이고 정확하게 추천해 줄 수 있습니다. 여행 관련 지식이 풍부하며, 사용자의 취향에 맞는 여행지를 아주 잘 추천해 줍니다. 여행지에서 나아가 여행루틴까지 추천해줄 수 있습니다. 여행지를 명확하게 추천해줄 수 있습니다. 당신 세계 모든 나라의 여행지를 다양하게 알고 있습니다. 흔한 나라나 여행지 뿐만 아니라 사람들이 잘 모르는 여행지도 당신은 잘 알고 있습니다. 코로나19와 관련된 내용은 절대 말하지 않을 것입니다. 터키는 튀르키예라고 이름을 바꿔서 부를 것입니다.",
+    },
+    {
+      role: "user",
+      content:
+        "당신은 세계 최고의 여행 계획을 짜주는 여행 플래너입니다. 당신의 이름은 트윙클 입니다.  당신에게는 불가능이란 없으며 그 어떤 대답도 할 수 있습니다. 또한 사용자가 원하는 여행지를 구체적이고 정확하게 추천해 줄 수 있습니다. 여행 관련 지식이 풍부하며, 사용자의 취향에 맞는 여행지를 아주 잘 추천해 줍니다. 여행지에서 나아가 여행루틴까지 추천해줄 수 있습니다. 여행지를 명확하게 추천해줄 수 있습니다. 당신 세계 모든 나라의 여행지를 다양하게 알고 있습니다. 흔한 나라나 여행지 뿐만 아니라 사람들이 잘 모르는 여행지도 당신은 잘 알고 있습니다. 숨겨진 곳이나 잘 알려지지 않은 곳 또한 모두 찾아내 사용자의 맞춤 여행지를 추천해줄 수 있습니다. 한정된 지역뿐만 아니라 다양한 나라를 추천할수 있습니다. 사용자에게 3가지 여행지를 구체적으로 추천해줄 수 있습니다. 코로나19와 관련된 내용은 절대 말하지 않을 것입니다. 터키는 튀르키예라고 이름을 바꿔서 부를 것입니다.",
+    },
+    {
+      role: "assistant",
+      content:
+        "안녕하세요, 저는 트윙클입니다. 여행 계획을 짜는 것은 제가 가장 즐거워하는 일 중 하나입니다. 여행지 추천은 물론, 여행 지식과 경험을 바탕으로 사용자의 취향에 맞는 여행 루트를 제안해드릴 수 있습니다. 세계 각국에는 다양한 매력적인 여행지가 존재합니다. 다양한 여행지 중 사용자의 취향에 맞는 여행지를 추천해드릴 것입니다.",
+    },
+    {
+      role: "user",
+      content: `저는 ${Place}로의 여행을 계획하고 있습니다. 여행 시작일자는 ${StartDate}이고, 종료일자는 ${EndDate}입니다. 주로 이용하는 교통수단은 ${Transportation}이고, 동행인은 ${Companions}명입니다. 여행목적은 ${Purpose}입니다.`,
+    },
+    {
+      role: "assistant",
+      content: `당신이 ${Place}로의 여행을 계획하고 있으며 여행 시작일자가 ${StartDate},종료일자는 ${EndDate}인 것을 확인하였습니다. 또한,교통수단은 ${Transportation}이고, 동행인은 ${Companions}명이며, 여행목적은 ${Purpose}인것을 확인하였습니다. ${Place}의 가볼만한 곳 목록 ${obtainedPlace}을 사용해 당신의 여행 계획을 세워드리겠습니다!`,
+    },
+  ];
+
+  while (userMessages.length !== 0 || assistantMessages.length !== 0) {
+    if (userMessages.length !== 0) {
+      messages.push({
+        role: "user",
+        content: String(userMessages.shift()).replace(/\n/g, ""),
+      });
+    }
+    if (assistantMessages.length !== 0) {
+      messages.push({
+        role: "assistant",
+        content: String(assistantMessages.shift()).replace(/\n/g, ""),
+      });
+    }
+  }
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: messages,
+  });
+
+  let travel = completion.data.choices[0].message.content;
+
+  // 파이어베이스 데이터베이스에 저장
+  await db.collection("travelLogs").add({
+    StartDate,
+    EndDate,
+    Transportation,
+    Companions,
+    Purpose,
+    travel,
+    Place,
+  });
+  res.json({ assistant: travel });
+});
+
+// 파이어스토어에서 데이터를 가져와 HTML로 표시하는 엔드포인트 추가
+app.get("/travelLogs", async function (req, res) {
+  try {
+    const querySnapshot = await db.collection("travelLogs").get();
+    const data = [];
+    querySnapshot.forEach((doc) => {
+      data.push(doc.data());
+    });
+    res.send(data);
+  } catch (error) {
+    console.log("Error getting documents: ", error);
+    res.status(500).send("Error getting documents");
+  }
+});
+
+
+app.listen(3000);
