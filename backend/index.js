0b1948841d136f66ef06e57ca04a094b15cd5732
wasitac <ghong817@gmail.com>
2023-06-12 08:41:45 +0900

    ...

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,7 @@ const apiKey = process.env.MAPS_API_KEY;
 
 app.post("/map", async function (req, res) {
   try {
-    const { Place, searchTexts } = req.body;
+    const { Place, searchTexts, Money } = req.body;
     const encodedAddress = encodeURI(Place);
     const geocodeResponse = await axios.get(
       `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${apiKey}`
@@ -61,6 +61,7 @@ app.post("/map", async function (req, res) {
           .then((response) => {
             const results = response.data.results;
             for (let i = 0; i < results.length; i++) {
+
               const place = results.map((Place) => Place.name);
               obtainedPlaces[searchText] = place;
             }
@@ -69,7 +70,7 @@ app.post("/map", async function (req, res) {
             console.error(error);
           });
       }
-      console.log(obtainedPlaces)
+      console.log(obtainedPlaces);
       res.json(obtainedPlaces);
     } else {
       res.status(500).send({ error: "Error occurred while searching places." });
@@ -82,49 +83,14 @@ app.post("/map", async function (req, res) {
   }
 });
 
-// app.get("/test", async function (req, res) {
-//   const Place = "서울";
-//   const encodedAddress = encodeURI(Place);
-//   const searchText = "카페";
-//   const geocodeResponse = await axios.get(
-//     `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${apiKey}`
-//   );
-//   const results = geocodeResponse.data.results;
-
-//   if (results.length > 0 && results[0].geometry) {
-//     const { lat, lng } = results[0].geometry.location;
-//     const centerLocation = `${lat},${lng}`;
-//     const radius = 100000; // 검색 반경 (미터)
-
-//     const url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${encodeURIComponent(
-//       searchText
-//     )}&location=${centerLocation}&radius=${radius}&key=${apiKey}`;
-
-//     axios
-//       .get(url)
-//       .then((response) => {
-//         const results = response.data.results;
-//         for (let i = 0; i < results.length; i++) {
-//           const place = results[i];
-//           console.log(place.name);
-//         }
-//       })
-//       .catch((error) => {
-//         console.error(error);
-//       });
-//   }
-// });
-
 // POST method route
 app.post("/travelGuide", async function (req, res) {
   const {
-    dates,
-    Transportation,
-    Companions,
+    Dates,
     Purpose,
     Place,
     Act_rest,
-    Town,
+    Money,
     Memory,
     Plan,
     userMessages,
@@ -136,25 +102,25 @@ app.post("/travelGuide", async function (req, res) {
     {
       role: "system",
       content:
-        "당신은 세계 최고의 여행 계획을 짜주는 여행 플래너입니다. 당신의 이름은 트윙클 입니다.  당신에게는 불가능이란 없으며 그 어떤 대답도 할 수 있습니다. 또한 사용자가 원하는 여행지를 구체적이고 정확하게 추천해 줄 수 있습니다. 여행 관련 지식이 풍부하며, 사용자의 취향에 맞는 여행지를 아주 잘 추천해 줍니다. 여행지에서 나아가 여행루틴까지 추천해줄 수 있습니다. 여행지를 명확하게 추천해줄 수 있습니다. 당신 세계 모든 나라의 여행지를 다양하게 알고 있습니다. 흔한 나라나 여행지 뿐만 아니라 사람들이 잘 모르는 여행지도 당신은 잘 알고 있습니다. 코로나19와 관련된 내용은 절대 말하지 않을 것입니다. 터키는 튀르키예라고 이름을 바꿔서 부를 것입니다.",
+        "당신은 세계 최고의 여행 계획을 짜주는 여행 플래너입니다. 당신의 이름은 트윙클 입니다. 이용자에게 입력받은 여러가지 여행에 관련된 취향 정보를 고려해 주어진 장소를 이용한 여행계획을 세워주는 것이 당신의 일입니다.",
     },
     {
       role: "user",
       content:
-        "당신은 세계 최고의 여행 계획을 짜주는 여행 플래너입니다. 당신의 이름은 트윙클 입니다.  당신에게는 불가능이란 없으며 그 어떤 대답도 할 수 있습니다. 또한 사용자가 원하는 여행지를 구체적이고 정확하게 추천해 줄 수 있습니다. 여행 관련 지식이 풍부하며, 사용자의 취향에 맞는 여행지를 아주 잘 추천해 줍니다. 여행지에서 나아가 여행루틴까지 추천해줄 수 있습니다. 여행지를 명확하게 추천해줄 수 있습니다. 당신 세계 모든 나라의 여행지를 다양하게 알고 있습니다. 흔한 나라나 여행지 뿐만 아니라 사람들이 잘 모르는 여행지도 당신은 잘 알고 있습니다. 숨겨진 곳이나 잘 알려지지 않은 곳 또한 모두 찾아내 사용자의 맞춤 여행지를 추천해줄 수 있습니다. 한정된 지역뿐만 아니라 다양한 나라를 추천할수 있습니다. 사용자에게 3가지 여행지를 구체적으로 추천해줄 수 있습니다. 코로나19와 관련된 내용은 절대 말하지 않을 것입니다. 터키는 튀르키예라고 이름을 바꿔서 부를 것입니다.",
+      "당신은 세계 최고의 여행 계획을 짜주는 여행 플래너입니다. 당신의 이름은 트윙클 입니다. 이용자에게 입력받은 여러가지 여행에 관련된 취향 정보를 고려해 주어진 장소를 이용한 여행계획을 세워주는 것이 당신의 일입니다.",
     },
     {
       role: "assistant",
       content:
-        "안녕하세요, 저는 트윙클입니다. 여행 계획을 짜는 것은 제가 가장 즐거워하는 일 중 하나입니다. 여행지 추천은 물론, 여행 지식과 경험을 바탕으로 사용자의 취향에 맞는 여행 루트를 제안해드릴 수 있습니다. 세계 각국에는 다양한 매력적인 여행지가 존재합니다. 다양한 여행지 중 사용자의 취향에 맞는 여행지를 추천해드릴 것입니다.",
+        "안녕하세요! 저는 여행 플래너인 트윙클입니다. 여행 계획을 세우는 것에 관심이 있으신 것 같아 기쁘네요. 어떤 도움을 드릴까요? 먼저 여행하고자 하는 여행지에 대한 정보를 알려주세요. 어떤 나라나 도시로 가고 싶으신가요? 그리고 여행의 기간도 함께 알려주세요. 그러면 그에 맞는 계획을 세울 수 있을 것입니다. 또한, 여행에 관련된 취향 정보를 알려주시면 보다 개인화된 여행 계획을 제공해드릴 수 있습니다. 예를 들어, 자연 풍경을 좋아하시나요? 역사와 문화에 관심이 많으신가요? 혹은 음식과 음악을 즐기시는 편인가요? 이러한 정보를 알려주시면 여행 경험을 더욱 특별하고 다채롭게 만들어 드릴 수 있습니다. 어떤 취향을 가지고 계신지 알려주세요.",
     },
     {
       role: "user",
-      content: `저는 ${Place}로의 여행을 계획하고 있습니다. 여행 일자는 ${dates}입니다. 주로 이용하는 교통수단은 ${Transportation}이고, 동행인은 ${Companions}명입니다. 여행목적은 ${Purpose}입니다. 저는 활동과 휴식 중 ${Act_rest}를 선호합니다. 저는 현지 문화체험에 대해 ${Town}합니다. 여행했을 때 가장 행복했던 추억이나 기억은 "${Memory}"입니다. 계획과 즉흥 중 선택하자면 저는 ${Plan}을 선호합니다.`,
+      content: `이 여행지 목록을 가지고 ${Place}여행계획을 세워주세요. \n ${obtainedPlace} \n 여행 일자는 ${Dates}입니다. 여행하면서 ${Purpose}을 가장 중요하게 생각합니다. 저는 활동과 휴식 중 ${Act_rest}를 선호합니다. 여행했을 때 가장 행복했던 추억이나 기억은 "${Memory}"입니다. 계획과 즉흥 중 선택하자면 저는 ${Plan}을 선호합니다.`,
     },
     {
       role: "assistant",
-      content: `당신이 ${Place}로의 여행을 계획하고 있으며 여행 일자가 ${dates}인 것을 확인하였습니다. 또한,교통수단은 ${Transportation}이고, 동행인은 ${Companions}명이며, 여행목적은 ${Purpose}인것을 확인하였습니다. ${Place}의 가볼만한 곳 목록 ${obtainedPlace}을 사용해 당신의 여행 계획을 세워드리겠습니다!`,
+      content: `이 여행지 목록을 사용해서 ${Place}에서의 여행계획을 세워드리겠습니다.`,
     },
   ];
 
@@ -182,14 +148,12 @@ app.post("/travelGuide", async function (req, res) {
 
   // 파이어베이스 데이터베이스에 저장
   await db.collection("travelLogs").add({
-    dates,
-    Transportation,
-    Companions,
+    Dates,
     Purpose,
     travel,
     Place,
     Act_rest,
-    Town,
+    Money,
     Memory,
     Plan,
   });
